b3d7454f0f028b8ecf8d70950420a783e04d2650
gauriket <gauriket@gmail.com>
2023-08-30 12:56:02 +0530

    hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Hero() {
+  return (
+    <Spline scene="https://prod.spline.design/90S-8iiU7GZh0VGM/scene.splinecode" />
+  );
+}
--- src/page/ho.jsx
+++ src/page/ho.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Detail,Card,Time} from '../components';
-import cover from '../ass/cover.jpg';
+// import cover from '../ass/cover.jpg';
 import "./ab.css";
+import Hero from '../components/Hero.jsx';
 
 const cardData = [
   [["1.Urinary Incontinence using machine learning", "2.Effect of Sanskrit verses and chants on the human brain and its healing capacities", "3.Brain signal collection and further refining method with the help of Neural Networks"], ["4.Medical Card","5.Solar tree classrooms", "6.EEG and EOG brain signal collection for concentration and anxiety level determination in soldiers during field work"," 7.Diagnosis of Scalp Disorders using Deep Learning Approach"],[" 8.Stock market determination with the help of MongoDB and Deep Learning and sentimental analysis"," 9.Movie recommendation software using Apache Spark framework and Databricks Community edition"," 10.Drive-eye"],[" 11.Conversion of natural language to SQL statements"," 12.Intelligent weed removal device with the help of image recognition"," 13.Depression detection with the help LLP and Twitter data"," 14.PHD tracking system"],[" 15.College inquiry chat bot"," 16.AI based lane detection system for automobiles"," 17.Gas-leakage and consumption detection in cylinders -IoT"," 18.Profanity-check on OTT platforms"," 19.Numerates Website"," 20.Peace Mate "]],
@@ -10,10 +11,11 @@ const ome = () => (
   <div className="ome">
     
     <div className='container'>
-    <img className='cover' src={cover} alt='loo' />
-    <div class="centered"><h1>INITIATE || IDEATE || INNOVATE</h1>
+      <Hero/>
+    {/* <img className='cover' src={cover} alt='loo' /> */}
+    {/* <div className="centered"><h1>INITIATE || IDEATE || INNOVATE</h1>
         <div><span className='snap l'>&lt;&lt;</span><p>we dont differentiate <br/>unless it’s <span style={{ color: '#F8C43F' }}>calculus</span></p><span className='snap r'>&gt;&gt;</span></div>
-    </div>
+    </div> */}
     </div>
     <div class="ll">
     <Time  events={[
@@ -25,7 +27,7 @@ const ome = () => (
       <Detail heading="<Notches on our belt>" hfsize="64px" hrmarl="485px" hrmarr="485px" spcolor="#F8C43F" wordColors={[{word:"17 major projects",scolor:"#F7E2AD"},
       {word:"3 research papers",scolor:"#F7E2AD"}]} 
 
-      description="<br><br>Numerates club believe in skill-set building through experience and<br> hard work. This club brought in various project and research works in<br> order to help its members in growing their understanding and<br> knowledge. In past one year, the club have undertaken 17 major projects<br> and 3 research papers have been published.
+      description="<br><br>We at Numerates believe in skill-set building through experience and<br> hard work. This club brought in various project and research work, in<br> order to help its members in growing their understanding and<br> knowledge. In the last year, the club has undertaken 17 major projects<br> and 3 research papers have been published.
       <br><br>"
       color="#fff"/>
 
